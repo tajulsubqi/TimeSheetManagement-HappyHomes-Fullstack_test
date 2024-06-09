3c4bd5ce3d880515295580cea4a56e3aa8e303f8
Tajul <subqitajul7@gmail.com>
2024-06-09 21:26:54 +0700

    chore: update dist

--- dist/data-source.js
+++ dist/data-source.js
@@ -1,18 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppDataSource = void 0;
-require("reflect-metadata");
-const typeorm_1 = require("typeorm");
+"use strict"
+Object.defineProperty(exports, "__esModule", { value: true })
+exports.AppDataSource = void 0
+require("reflect-metadata")
+const typeorm_1 = require("typeorm")
 exports.AppDataSource = new typeorm_1.DataSource({
-    type: "postgres",
-    host: "monorail.proxy.rlwy.net",
-    port: 25114,
-    username: "postgres",
-    password: "OoNabkeECEAgiiOomFzTyjzSjpNGiGPn",
-    database: "railway",
-    synchronize: false,
-    logging: false,
-    entities: ["src/entity/**/*.ts"],
-    migrations: ["src/migration/**/*.ts"],
-    subscribers: [],
-});
+  type: "postgres",
+  host: "monorail.proxy.rlwy.net",
+  port: 25114,
+  username: "postgres",
+  password: "OoNabkeECEAgiiOomFzTyjzSjpNGiGPn",
+  database: "railway",
+  synchronize: false,
+  logging: false,
+  entities: ["dist/entity/**/*.js"],
+  migrations: ["dist/migration/**/*.js"],
+  subscribers: [],
+})
