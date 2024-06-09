c2a9e5a51c97e536d1fa1329e56e06717187fd1c
Tajul <subqitajul7@gmail.com>
2024-06-09 21:31:11 +0700

    chore: update dist

--- dist/data-source.js
+++ dist/data-source.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true })
 exports.AppDataSource = void 0
 require("reflect-metadata")
 const typeorm_1 = require("typeorm")
+require("dotenv/config")
 exports.AppDataSource = new typeorm_1.DataSource({
   type: "postgres",
-  host: "monorail.proxy.rlwy.net",
-  port: 25114,
-  username: "postgres",
-  password: "OoNabkeECEAgiiOomFzTyjzSjpNGiGPn",
-  database: "railway",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
   entities: ["dist/entity/**/*.js"],
